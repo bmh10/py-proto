5b122bfc3c3569f2a6a5bf9d6bd4cb0b37b38a1b
bmh10 <bmhomer13@gmail.com>
2017-12-03 11:41:27 +0000

    R/W binary test

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 import addressbook_pb2
 
-person = addressbook_pb2.Person()
-person.id = 1234
-person.name = "John Doe"
-person.email = "jdoe@example.com"
-phone = person.phones.add()
-phone.number = "555-4321"
-phone.type = addressbook_pb2.Person.HOME
-print IsInitialized()
+def create_person():
+  person = addressbook_pb2.Person()
+  person.id = 1
+  person.name = "John Doe"
+  person.email = "jdoe@example.com"
+  phone = person.phones.add()
+  phone.number = "555-4321"
+  phone.type = addressbook_pb2.Person.HOME
+  return person
+
+def write(obj):
+  f = open("out", "wb")
+  f.write(obj.SerializeToString())
+  f.close()
+
+def read():
+  f = open("out", "rb")
+  p = addressbook_pb2.Person()
+  p.ParseFromString(f.read())
+  f.close()
+  return p
+
+p = create_person()
+write(p)
+p = read()
+print p
